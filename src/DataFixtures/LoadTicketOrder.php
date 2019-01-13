f014aca8f3dccccfe88b7b44d23e9ab7a7880454
dlaxcess <flipiste@free.fr>
2019-01-13 19:40:38 +0100

    modification des dépendances des fixture avec l'implementation de l interface DependentFixtureInterface dans LoadTicketOrder pour qu'il soit chargé en drenier

--- src/DataFixtures/LoadTicketOrder.php
+++ src/DataFixtures/LoadTicketOrder.php
@@ -11,43 +11,55 @@ namespace App\DataFixtures;
 
 use App\Entity\Ticket;
 use App\Entity\TicketOrder;
+use App\Repository\DiscountsRepository;
+use App\Repository\DurationRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\ORM\EntityManagerInterface;
 
-class LoadTicketOrder extends Fixture
+
+class LoadTicketOrder extends Fixture implements DependentFixtureInterface
 {
     private $discountRepository;
     private $durationRepository;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(DiscountsRepository $discountRepo, DurationRepository $durationRepo)
     {
-        $this->discountRepository = $em->getRepository('App:Discounts');
-        $this->durationRepository = $em->getRepository('App:Duration');
+        $this->discountRepository = $discountRepo;
+        $this->durationRepository = $durationRepo;
     }
 
     public function load(ObjectManager $manager)
     {
-//        $duration = $this->durationRepository->find(1);
-//        for ($i = 0; $i <= 998; $i++) {
-//
-//            $ticket = new Ticket();
-//
-//            $ticket->setVisitorFirstName('zertyu');
-//            $ticket->setVisitorName('yhgvcd');
-//            $ticket->setVisitorBirthDate(new \DateTime('1980-01-20 00:00:00'));
-//            $ticket->setDiscount($this->discountRepository->find(1));
-//            $ticket->setCountry('FR');
-//
-//            $TicketOrderEntry = new TicketOrder();
-//
-//            $TicketOrderEntry->setVisitDate(new \DateTime('2019-01-20 00:00:00'));
-//            $TicketOrderEntry->setDuration($duration);
-//            $TicketOrderEntry->setBookingEmail('flipiste@free.fr');
-//            $TicketOrderEntry->addTicket($ticket);
-//
-//            $manager->persist($TicketOrderEntry);
-//        }
-//        $manager->flush();
+        $duration = $this->durationRepository->find(1);
+        for ($i = 0; $i <= 996; $i++) {
+
+            $ticket = new Ticket();
+
+            $ticket->setVisitorFirstName('zertyu');
+            $ticket->setVisitorName('yhgvcd');
+            $ticket->setVisitorBirthDate(new \DateTime('1980-01-20 00:00:00'));
+            $ticket->setDiscount($this->discountRepository->find(1));
+            $ticket->setCountry('FR');
+
+            $TicketOrderEntry = new TicketOrder();
+
+            $TicketOrderEntry->setVisitDate(new \DateTime('2019-01-21 00:00:00'));
+            $TicketOrderEntry->setDuration($duration);
+            $TicketOrderEntry->setBookingEmail('flipiste@free.fr');
+            $TicketOrderEntry->addTicket($ticket);
+
+            $manager->persist($TicketOrderEntry);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            LoadAgesPrices::class,
+            LoadDiscounts::class,
+            LoadDuration::class,
+        );
     }
 }
