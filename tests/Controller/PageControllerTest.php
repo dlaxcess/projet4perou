2a2756f1d1a010cd52d517387bb8ba87f12401f7
dlaxcess <flipiste@free.fr>
2019-03-03 18:29:16 +0100

    Creation du test fonctionnel verifiant l'affichage de la page de billeterie

---
+++ tests/Controller/PageControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dlaxcess
+ * Date: 03/03/19
+ * Time: 18:03
+ */
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testBilleterie()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/billetterie');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
